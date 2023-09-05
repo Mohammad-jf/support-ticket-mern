fecba80fa82fdda2e284f843b84e78093db08633
mohammad jalifathi <m.h.jalifathi@gmail.com>
2023-09-05 12:37:27 +0330

    error midlleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,27 @@
+// @desc Registera new user
+// @route /api/users
+// @access public
+
 const registerUser = (req, res) => {
+  const { name, email, password } = req.body;
+
+  // validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("please include all fields");
+  }
+
   res.send("Register Route");
 };
 
+// @desc login a user
+// @route /api/users/login
+// @access public
 const loginUser = (req, res) => {
   res.send("login Route");
 };
 
-
 module.exports = {
-    registerUser,
-    loginUser
-}
+  registerUser,
+  loginUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const app = express();
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "wellcome to the support desk api" });
@@ -9,5 +13,7 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
+// error handler
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
