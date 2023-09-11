6adcad02d3910167a32fb6748959c20334bbab28
mohammad jalifathi <m.h.jalifathi@gmail.com>
2023-09-11 05:25:43 +0330

    frontend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "../src/pages/Home";
+import Login from "../src/pages/Login";
+import Register from "../src/pages/Register";
+import Header from "./components/Header";
+
+
 function App() {
-  return <div>my app</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  )
+
 }
 
 export default App;
