69b5156a7a0362416352e554d1cd0587c7d042ee
mohammad jalifathi <m.h.jalifathi@gmail.com>
2023-09-15 03:49:18 +0330

    ticket controller

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,7 +7,17 @@ const Ticket = require("../models/ticketModel");
 // @access privete
 
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get tickets" });
+  // get user using the id & jwt
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("user not found");
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+
+  res.status(200).json(tickets);
 });
 
 // @desc create new ticket
@@ -15,6 +25,11 @@ const getTickets = asyncHandler(async (req, res) => {
 // @access privete
 
 const createTicket = asyncHandler(async (req, res) => {
+  const { product, description } = req.body;
+  if (!product || !description) {
+    res.status(400);
+    throw new Error("please add a product & description");
+  }
   res.status(200).json({ message: "create ticket" });
 });
 
